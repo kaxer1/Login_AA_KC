92f8ea7420396af9751821e40f3b750e149e9dd9
kaxer1 <kevincuray41@gmail.com>
2019-10-22 18:56:31 -0500

    commit login

--- app/src/main/java/com/example/login_aa_kc/MainActivity.java
+++ app/src/main/java/com/example/login_aa_kc/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.login_aa_kc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txtUsu;
+    private EditText txtPass;
+    private TextView login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void login(View v){
+
+        txtUsu = (EditText)findViewById(R.id.txtUsuario);
+        txtPass = (EditText) findViewById(R.id.txtPassword);
+
+
+        if(txtUsu.getText().toString().equals("admin") && txtPass.getText().toString().equals("admin")){
+
+            login = (TextView) findViewById(R.id.txt);
+            login.setText("Ingreso Correcto");
+
+
+        }else{
+            //wrong password
+        }
+    }
+
 }
